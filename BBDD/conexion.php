b2164552c6a10e49a958104533b240fc7e7e499b
GTCP <gtcp.solutions@gmail.com>
2019-09-29 13:29:34 -0300

    Primer Prototipo

---
+++ BBDD/conexion.php
@@ -0,0 +1,7 @@
+<?php   
+    $servername = "localhost";
+    $username = "pma";
+    $password = "";
+
+    $conexion = new mysqli($servername, $username, $password) or die ("No se pudo realizar la conexion");    
+?>
