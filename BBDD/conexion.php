0572f3388347c6b2c2df91222c040ebea54c97d0
Exequiel Hernando <exequielhernando@gmail.com>
2019-10-09 18:35:16 -0300

    Rama creada y master limpio

--- BBDD/conexion.php
+++
@@ -1,7 +0,0 @@
-<?php   
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-
-    $conexion = new mysqli($servername, $username, $password) or die ("No se pudo realizar la conexion");    
-?>
